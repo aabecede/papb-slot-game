19d312c7cc284078d828c0f5d1d3da78bb138154
aabecede <aabecede>
2023-05-01 16:41:06 +0700

    Slot 3 Image

--- app/src/main/java/com/example/slotgame/MainActivity.java
+++ app/src/main/java/com/example/slotgame/MainActivity.java
@@ -10,43 +10,57 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    Thread thrd;
-    Handler hndler;
-
-    int id = 1;
-    private ImageView imgSlot;
+    private ImageView[] imgSlots = new ImageView[3];
     private Button btnStartStop;
+    int id = 1;
+    private boolean[] isSpinning = new boolean[3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.imgSlot = this.findViewById(R.id.imgSlot);
-        this.btnStartStop = this.findViewById(R.id.btnStop);
-        this.btnStartStop.setOnClickListener(this);
-        this.hndler = new Handler(Looper.getMainLooper());
-
+        imgSlots[0] = findViewById(R.id.imgSlot);
+        imgSlots[1] = findViewById(R.id.imgSlot2);
+        imgSlots[2] = findViewById(R.id.imgSlot3);
+        btnStartStop = findViewById(R.id.btnStartStop);
+        btnStartStop.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-
-        if (this.thrd != null && this.thrd.isAlive()) {
-            this.thrd.interrupt();
+        // Check which image view to stop spinning
+        int slotToStop = -1;
+        for (int i = 0; i < 3; i++) {
+            if (isSpinning[i]) {
+                slotToStop = i;
+                break;
+            }
         }
-        this.thrd = new Thread(new Runnable() {
+
+        // If all image views are stopped, start spinning them all
+        if (slotToStop == -1) {
+            for (int i = 0; i < 3; i++) {
+                startSpinning(imgSlots[i], i);
+            }
+        }
+        // Otherwise, stop the next image view in the array
+        else {
+            stopSpinning(imgSlots[slotToStop]);
+            isSpinning[slotToStop] = false;
+        }
+    }
+
+    private void startSpinning(final ImageView imgSlot, final int slotIndex) {
+        isSpinning[slotIndex] = true;
+        new Thread(new Runnable() {
             @Override
             public void run() {
-                // kode disini akan dijalankan di thread terpisah
-                while (true) {
-                    if (id == 9)
-                        id = 1;
-                    else
-                        id++;
+                while (isSpinning[slotIndex]) {
+                    if (id == 9) id = 1;
+                    else id++;
                     try {
-                        hndler.post(new Runnable() {
+                        runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
                                 imgSlot.setImageResource(Helper.getIcon(id));
@@ -55,13 +69,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         Thread.sleep(100);
                     } catch (InterruptedException e) {
                         break;
-//                        throw new RuntimeException(e);
                     }
                 }
-
-
             }
-        });
-        this.thrd.start();
+        }).start();
+    }
+
+    private void stopSpinning(final ImageView imgSlot) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(100); // Wait for half a second before stopping the spinning
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        // Set the image to the final icon
+                        imgSlot.setImageResource(Helper.getIcon(id));
+                    }
+                });
+            }
+        }).start();
     }
 }
